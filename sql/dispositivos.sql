fba851de5b203547aba2fe1a8a76990274282ba6
JoseDavidEsquivel <122810644+JoseDavidEsquivel@users.noreply.github.com>
2023-11-27 09:44:52 -0600

    modificacion de la abse de datos

--- sql/dispositivos.sql
+++ sql/dispositivos.sql
@@ -1,11 +1,10 @@
 CREATE TABLE dispositivos (
     id          INTEGER PRIMARY KEY,
     dispositivo VARCHAR(100),
-    led         INTEGER CHECK (led >= 0 AND led <= 1),
-    sensor      INTEGER DEFAULT 0
+    valor      INTEGER DEFAULT 0
 );
 
-INSERT INTO dispositivos (id, dispositivo, led, sensor)
-VALUES (1, 'ESP32', 0, 0);
+INSERT INTO dispositivos (id, dispositivo)
+VALUES (1, 'ESP32');
 
 SELECT * FROM dispositivos;
